02ac2608f601181ec94952490e8328465773c9a4
AERO <aeroassault6@gmail.com>
2023-05-23 18:04:22 -0400

    Initial commit of DeepSpaceTrading

---
+++ src/deepspacetrading/DeepSpaceModPlugin.java
@@ -0,0 +1,21 @@
+package deepspacetrading;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import deepspacetrading.util.DeepSpaceTariffUtil;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class DeepSpaceModPlugin extends BaseModPlugin {
+
+    private static final Logger log = Global.getLogger(DeepSpaceModPlugin.class);
+    static {log.setLevel(Level.ALL);}
+
+    public static final String ID = "deep_space_trading";
+
+    @Override
+    public void onGameLoad(boolean newGame) {
+        new DeepSpaceTariffUtil();
+        log.info("DeepSpaceTrading eventListener is loaded.");
+    }
+}
---
+++ src/deepspacetrading/util/DeepSpaceTariffUtil.java
@@ -0,0 +1,36 @@
+package deepspacetrading.util;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.BaseCampaignEventListener;
+import com.fs.starfarer.api.campaign.econ.MarketAPI;
+import deepspacetrading.DeepSpaceModPlugin;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class DeepSpaceTariffUtil extends BaseCampaignEventListener {
+
+    private static final Logger log = Global.getLogger(DeepSpaceTariffUtil.class);
+    static {log.setLevel(Level.ALL);}
+
+    public DeepSpaceTariffUtil() {
+        super(false);
+        Global.getSector().addTransientListener(this);
+    }
+
+    @Override
+    public void reportPlayerOpenedMarket(MarketAPI market) {
+        if (market.isHidden()) {
+            market.getTariff().modifyMult(DeepSpaceModPlugin.ID,0f);
+            log.info("Modified hidden market tariff.");
+        }
+    }
+
+    @Override
+    public void reportPlayerClosedMarket(MarketAPI market) {
+        if (market.isHidden()) {
+            market.getTariff().unmodify(DeepSpaceModPlugin.ID);
+            log.info("Unmodified hidden market tariff.");
+        }
+    }
+
+}
